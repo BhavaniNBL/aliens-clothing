bf29bfbb6de9fa3b4c0036bfc96d1f0221acd089
BhavaniNBL <nblbhavani1995@gmail.com>
2021-05-18 00:54:36 +0530

    added styles and components

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,40 +1,11 @@
 import React from 'react';
+import Directory from '../../components/directory/directory.component';
+
 import './homepage.styles.scss';
 
 const Homepage = () => (
     <div className="homepage">
-      <div className="directory-menu">
-          <div className='menu-item'>
-              <div className='content'>
-                  <h1 className='title'>HATS</h1>
-                  <span className='subtitle'>SHOP NOW</span>
-              </div>
-          </div>
-          <div className='menu-item'>
-              <div className='content'>
-                  <h1 className='title'>sneakers</h1>
-                  <span className='subtitle'>SHOP NOW</span>
-              </div>
-          </div>
-          <div className='menu-item'>
-              <div className='content'>
-                  <h1 className='title'>beanies</h1>
-                  <span className='subtitle'>SHOP NOW</span>
-              </div>
-          </div>
-          <div className='menu-item'>
-              <div className='content'>
-                  <h1 className='title'>mens</h1>
-                  <span className='subtitle'>SHOP NOW</span>
-              </div>
-          </div>
-          <div className='menu-item'>
-              <div className='content'>
-                  <h1 className='title'>womens</h1>
-                  <span className='subtitle'>SHOP NOW</span>
-              </div>
-          </div>
-      </div>
+      <Directory />
     </div>
 )
 export default Homepage;
